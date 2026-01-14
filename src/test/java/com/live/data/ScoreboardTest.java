1f911b0091926c74b8ab33870c05bc5a3c200e32
rudiuknastya <ruduknasta13@gmail.com>
2026-01-14 15:55:16 +0100

    Created test for start game

---
+++ src/test/java/com/live/data/ScoreboardTest.java
@@ -0,0 +1,31 @@
+package com.live.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ScoreboardTest {
+    private Scoreboard scoreboard;
+    private Team homeTeam;
+    private Team awayTeam;
+
+    @BeforeEach
+    void setUp() {
+        scoreboard = new Scoreboard();
+        homeTeam = new Team("homeTeam");
+        awayTeam = new Team("awayTeam");
+    }
+
+    @Test
+    void shouldStartGame(){
+        Long gameId = scoreboard.startGame(homeTeam, awayTeam);
+        assertNotNull(gameId);
+
+        List<Game> summary = scoreboard.getSummary();
+        assertEquals(1, summary.size());
+    }
+
+
+}
